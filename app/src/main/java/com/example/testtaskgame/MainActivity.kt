820fd4aa7b73551e998c3917d11c12d490407281
TruePelmen <oleksii.senyshyn@lnu.edu.ua>
2024-05-27 09:05:58 +0300

    Incommitted splash screen

--- app/src/main/java/com/example/testtaskgame/MainActivity.kt
+++ app/src/main/java/com/example/testtaskgame/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.testtaskgame
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.testtaskgame.navigation.AppNavigation
 import com.example.testtaskgame.services.MusicService
 
@@ -13,38 +19,38 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val viewModel by viewModels<MainViewModel>()
         super.onCreate(savedInstanceState)
-//        installSplashScreen().apply {
-//            setKeepOnScreenCondition {
-//                viewModel.isLoading.value
-//            }
-//            setOnExitAnimationListener { screen ->
-//                val pulseX = ObjectAnimator.ofFloat(
-//                    screen.iconView,
-//                    View.SCALE_X,
-//                    1.0f,
-//                    1.5f,
-//                    1.0f
-//                )
-//                pulseX.interpolator = OvershootInterpolator()
-//                pulseX.duration = 1000L
-//
-//                val pulseY = ObjectAnimator.ofFloat(
-//                    screen.iconView,
-//                    View.SCALE_Y,
-//                    1.0f,
-//                    1.5f,
-//                    1.0f
-//                )
-//                pulseY.interpolator = OvershootInterpolator()
-//                pulseY.duration = 1000L
-//
-//                val animatorSet = AnimatorSet()
-//                animatorSet.playTogether(pulseX, pulseY)
-//                animatorSet.doOnEnd { screen.remove() }
-//
-//                animatorSet.start()
-//            }
-//        }
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+            setOnExitAnimationListener { screen ->
+                val pulseX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    1.0f,
+                    1.5f,
+                    1.0f
+                )
+                pulseX.interpolator = OvershootInterpolator()
+                pulseX.duration = 1000L
+
+                val pulseY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    1.0f,
+                    1.5f,
+                    1.0f
+                )
+                pulseY.interpolator = OvershootInterpolator()
+                pulseY.duration = 1000L
+
+                val animatorSet = AnimatorSet()
+                animatorSet.playTogether(pulseX, pulseY)
+                animatorSet.doOnEnd { screen.remove() }
+
+                animatorSet.start()
+            }
+        }
         startMusicService()
 
         enableEdgeToEdge()
